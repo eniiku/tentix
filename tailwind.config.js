363cd7ec151d5b6c57011dc5db57dc685dcddf6e
David Enikuomehin <enikuauth@gmail.com>
2023-09-05 06:42:09 +0100

    feat: create sidebar comp & import required assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      colors: {
+        gray: {
+          400: '#98A2B3',
+        },
+        pry: {
+          blue: {
+            light: '#009DE0',
+            dark: '#354B9E',
+          },
+          gray: {
+            border: '#F0F0F0',
+          },
+        },
+        sec: {
+          main: '#141522',
+        },
+      },
+    },
   },
   plugins: [],
 };
