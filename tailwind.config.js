049e332c0f3f96bf0a6868a2a98f9d4e51f86043
David Enikuomehin <enikuauth@gmail.com>
2023-09-05 07:18:09 +0100

    feat: create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,25 @@ module.exports = {
       },
       colors: {
         gray: {
+          200: '#EAECFO',
           400: '#98A2B3',
+          500: '#667085',
+          700: '#344054',
         },
         pry: {
           blue: {
             light: '#009DE0',
             dark: '#354B9E',
+            alt: '#369FFF',
           },
-          gray: {
-            border: '#F0F0F0',
+          border: {
+            100: '#F0F0F0',
+            200: '#DDE2E4',
           },
         },
         sec: {
           main: '#141522',
+          darkGrey: '#1B2559',
         },
       },
     },
